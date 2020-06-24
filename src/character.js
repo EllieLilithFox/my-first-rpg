77770b64a6eee41be5074994bc6febc60c2c1570
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-24 11:26:21 -0700

    first test passes again
    Co-authored-by: Jeff Kim <kim2jy@hotmail.com>

--- src/character.js
+++ src/character.js
@@ -2,7 +2,7 @@ export class Character {
   constructor(name, hp) {
     this.name = name;
     this.hp = hp;
-    this.xp = "";
+    this.xp = 0;
     this.attributes = {
       str: 10,
       dex: 10,
